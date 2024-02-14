b8a7dbbde5fbc676590a6175f271925e317b0fb5
ugc-padlet <160005455+ugc-padlet@users.noreply.github.com>
2024-02-14 17:29:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+﻿document.getElementById('quiz-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const answers = {
+        question1: 'maru',
+        question2: 'maru',
+        question3: 'maru',
+        question4: 'maru',
+        question5: 'maru',
+    };
+
+    let score = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        const questionName = 'question' + i;
+        const selectedAnswer = document.querySelector('input[name="' + questionName + '"]:checked');
+
+        if (selectedAnswer) {
+            if (selectedAnswer.value === answers[questionName]) {
+                score++;
+            }
+        }
+    }
+
+    alert('あなたの正解した問題は ' + score + '/5 です。');
+});
+
+window.addEventListener('scroll', () => {
+    const pageTop = document.getElementById('page-top'); //トップに戻るボタンのidを取得
+    if (window.pageYOffset >= 400) {  //上から400px以上スクロールしたら
+        pageTop.classList.add('fadein'); //aタグにfadeinクラスを与える
+    } else {
+        pageTop.classList.remove('fadein');//aタグからfadeinクラスをはずす
+    }
+});
